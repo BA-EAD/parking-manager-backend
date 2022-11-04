430625ed98d8524d810076de2f45d1e711b5ba09
Bhavin Patel <bhavin.patel@manektech.com>
2022-11-04 18:27:15 +0530

    Added test cases, rate limiter, and secure routes with jwt.

--- server.js
+++ server.js
@@ -1,34 +1,9 @@
-const express = require('express');
-const Cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
-
+const app = require('./app');
 const logger = require('./src/helpers/logger.helper');
-const { successResponse } = require('./src/helpers/response.helper');
 
-const app = express();
-app.use(Cors());
-
-app.use(
-  bodyParser.urlencoded({
-    parameterLimit: 100000,
-    limit: '50mb',
-    extended: true,
-  })
-);
-app.use(bodyParser.json());
-
-dotenv.config();
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  return successResponse(res, 'PARKING MANAGER BACKEND APIs.');
-});
-
-require('./src/routes/parking.routes')(app);
-
 const server = app.listen(port, function () {
-  console.log(`Express server listening on port ${port}`);
   logger.info(`Express server listening on port ${port}`);
 });
 
--- src/routes/parking.routes.js
+++ src/routes/parking.routes.js
@@ -1,10 +1,11 @@
-module.exports = (app) => {
-  const router = require('express').Router();
-  const Parking = require('../controller/parking.controller');
+const { authenticateRequest } = require('../middlewares/passport.middleware');
+const router = require('express').Router();
+const Parking = require('../controller/parking.controller');
 
-  router.get('/slot', Parking.checkSlot);
-  router.post('/park', Parking.parkCar);
-  router.post('/unpark', Parking.unparkCar);
+module.exports = (app) => {
+  router.get('/slot', authenticateRequest, Parking.checkSlot);
+  router.post('/park', authenticateRequest, Parking.parkCar);
+  router.post('/unpark', authenticateRequest, Parking.unparkCar);
 
   app.use('/', router);
 };
